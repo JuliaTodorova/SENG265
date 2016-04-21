d6a2047b8567f57d0b038df68fb0746a18cf82ec
JuliaTodorova <juliastoycheva@gmail.com>
2016-04-21 14:46:04 -0700

    Created method that reverses a string

--- main.c
+++ main.c
@@ -6,5 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
+char *strrev(char *str) {
+	if (str == NULL){
+		return -1;
+	}
+
+	int len = strlen(str);	
+	char *newString = (char*)malloc((len + 1)* sizeof(char));
+	
+	for (int i = 0; i < len; i++){
+		newString[i] = str[len - i - 1];
+	}
+	
+	return newString;
+}
 
 
